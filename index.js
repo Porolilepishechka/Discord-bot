78eade7987b8f18a1a86fea32cc2c5c47928f25b
Porolilepishechka <denispalaguta2018@gmail.com>
2025-03-16 23:32:24 +0200

    DBK-1 search forbidden Words inplementation

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ const client = new Client({
     ]
 });
 
+const forbiddenWords = ['хуй', 'пизда'];
+
 client.once('ready', () => {
     console.log(`✅ Бот запущено як ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
     if (message.author.bot) return;
+
+    if (forbiddenWords.some(word => message.content.toLowerCase().replace(/\s/g, '').trim().includes(word))) {
+        message.delete().catch(console.error);
+        message.channel.send(`${message.author}, твій меседж містить заборонене слово! 🚫`);
+        return;
+    }
 
     if (message.content.toLowerCase() === 'привіт') {
         message.reply('Привіт! 👋');
